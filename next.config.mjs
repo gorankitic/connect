fd9be9199fe46e8e37b74fbf22f2cc29f6831d5a
unknown <kiticgoran90@gmail.com>
2024-03-10 13:06:29 +0100

    next config update

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,4 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//     images: {
-//         remotePatterns: [
-//           {
-//             protocol: 'https',
-//             hostname: "utfs.io",
-//             pathname: '**',
-//           },
-//         ],
-//       },
-// };
-
-
-import { expand } from "dotenv-expand";
-
-expand({ parsed: { ...process.env } });
-
-/**
- * @type {import('next').NextConfig}
- */
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
